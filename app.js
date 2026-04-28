653cac309868a944281a528a711120a71e0779e0
BManojh <manojhb7640@gmail.com>
2026-04-28 21:29:46 +0530

    mu4

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-// serve static files
-app.use(express.static(__dirname));
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// correct path
+// Default route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(3000, () => {
